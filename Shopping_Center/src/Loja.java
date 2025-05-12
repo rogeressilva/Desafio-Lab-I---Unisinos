92236bc5845d7adc58b4ee6dbd094c7e2a30a43b
Rógeres <rogeress72@gmail.com>
2025-05-12 20:11:07 -0300

    adiciona arquivos de configuração e estrutura inicial do projeto Java

---
+++ Shopping_Center/src/Loja.java
@@ -0,0 +1,16 @@
+public class Loja {
+
+    // Atributos
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    // Construtor I
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+
+}
