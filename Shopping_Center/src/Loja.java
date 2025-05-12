690a3e75fd044af64eb104fbfb05ad06e2ac0295
Rógeres <rogeress72@gmail.com>
2025-05-12 20:55:12 -0300

    refatora a formatação do código na classe Loja para melhorar a legibilidade

--- Shopping_Center/src/Loja.java
+++ Shopping_Center/src/Loja.java
@@ -1,68 +1,68 @@
 public class Loja {
 
-    // Atributos
-    private String nome;
-    private int quantidadeFuncionarios;
-    private double salarioBaseFuncionario;
 
-    
-    // Construtor I - Começa tudo
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
-        this.nome = nome;
-        this.quantidadeFuncionarios = quantidadeFuncionarios;
-        this.salarioBaseFuncionario = salarioBaseFuncionario;
-    }
+     // Atributos
+     private String nome;
+     private int quantidadeFuncionarios;
+     private double salarioBaseFuncionario;
 
-    // Construtor II - Somente o nome e a quantidade de funcionarios
+        
+     // Construtor I - Começa tudo
+     public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+         this.nome = nome;
+         this.quantidadeFuncionarios = quantidadeFuncionarios;
+         this.salarioBaseFuncionario = salarioBaseFuncionario;
+     }
+
+     // Construtor II - Somente o nome e a quantidade de funcionarios
      public Loja(String nome, int quantidadeFuncionarios) {
-        this.nome = nome;
-        this.quantidadeFuncionarios = quantidadeFuncionarios;
-        this.salarioBaseFuncionario = -1;
-    }
+         this.nome = nome;
+         this.quantidadeFuncionarios = quantidadeFuncionarios;
+         this.salarioBaseFuncionario = -1;
+     }
 
-    //Getters e Setters
-    public String getNome() {
+     //Getters e Setters
+     public String getNome() {
         return nome;
-    }
+     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
 
-    public int getQuantidadeFuncionarios() {
+     public int getQuantidadeFuncionarios() {
         return quantidadeFuncionarios;
-    }
+     }
 
-    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+     public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
         this.quantidadeFuncionarios = quantidadeFuncionarios;
-    }
+     }
 
-    public double getSalarioBaseFuncionario() {
-        return salarioBaseFuncionario;
-    }
+     public double getSalarioBaseFuncionario() {
+         return salarioBaseFuncionario;
+     }
 
-    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
-        this.salarioBaseFuncionario = salarioBaseFuncionario;
-    }
+       public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+         this.salarioBaseFuncionario = salarioBaseFuncionario;
+     }
 
-    // Método para calcular o salário total dos funcionários
-    public double calcularSalarioTotalFuncionarios() {
-        if (salarioBaseFuncionario < 0) {
+     // Método para calcular o salário total dos funcionários
+     public double calcularSalarioTotalFuncionarios() {
+         if (salarioBaseFuncionario < 0) {
             return -1; // Retorna -1 se o salário base não foi definido
-        }
-        return quantidadeFuncionarios * salarioBaseFuncionario; //Calcula o gasto total com salários
-    }
+         }
+          return quantidadeFuncionarios * salarioBaseFuncionario; //Calcula o gasto total com salários
+     }
 
-    // Método para verificar o tamanho da loja
-    public char tamanhoDaLoja () {
-        if (quantidadeFuncionarios < 10) {
+     // Método para verificar o tamanho da loja
+     public char tamanhoDaLoja () {
+         if (quantidadeFuncionarios < 10) {
             return 'P';
-        } else if (quantidadeFuncionarios >= 10 && quantidadeFuncionarios <= 50) {
-            return 'M';
-        } else {
-            return 'G';
-        }
-
-    }
-
+         } else if (quantidadeFuncionarios >= 10 && quantidadeFuncionarios <= 50) {
+             return 'M';
+         } else {
+             return 'G';
+         }
+    }     
+    
 }
